31f98ccb6de54cef7edf398a158e78f7f0c5c0e6
Ilyas Agadilov <agadilov.ilyas@mail.ru>
2026-02-16 16:21:15 +0500

    Научился как ЛЛМ предсказывают следующий токен

--- data-sampling.py
+++ data-sampling.py
@@ -10,4 +10,22 @@ with open("the-verdict.txt", "r") as file:
 encoded_text = tokenizer.encode(raw_text)
 # Зашифровали текст из вердикт ткст в инпут токены
 
-print(len(encoded_text))
+# print(len(encoded_text))
+# убрали первые 50 токенов
+enc_sample = encoded_text[50:]
+
+context_size = 4 # контекст определяет сколько токенов будет использовано для инпута 
+# x = enc_sample[:context_size]
+# y = enc_sample[1:context_size+1]
+# print(f"x: {x}")
+# print(f"y: {y}")
+
+# весь сок - предсказываем следующий токен
+for i in range(1, context_size+1):
+  # print(f"Context size: {i}")
+  context = enc_sample[:i]
+  desired = enc_sample[i]
+  print(f"{context} ----> {desired}")
+  print(f"{tokenizer.decode(context)} ----> {tokenizer.decode([desired])}")
+
+  
